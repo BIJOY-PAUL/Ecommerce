e4f1866df982f4df3744166021b8fcb545975c54
Bijoy rudra paul <bijoypal4856@gmail.com>
2025-04-05 11:36:55 +0600

    home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce/pages/home.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,8 +10,6 @@ class myApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: ,
-    );
+    return MaterialApp(home: homeScreen());
   }
 }
