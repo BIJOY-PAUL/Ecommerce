2652e44696584a1c3d8cb17dc967c46cd785692d
Bijoy rudra paul <bijoypal4856@gmail.com>
2025-04-07 11:10:15 +0600

    menu add

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -6,14 +6,7 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-      
-
-
-
-
-
-
+      body: Container(child: Column(children: [Icon(Icons.menu)])),
     );
   }
 }
