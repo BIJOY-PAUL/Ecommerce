33355d256379f1e3d63e2c1137aa5eada4ef349a
Bijoy rudra paul <bijoypal4856@gmail.com>
2025-04-08 10:03:27 +0600

    listview image add

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -49,22 +49,152 @@ class Home extends StatelessWidget {
             ),
 
             Container(
-              height: 120,
+              height: 90,
               child: ListView(
+                scrollDirection: Axis.horizontal,
                 children: [
                   Container(
-                    decoration: BoxDecoration(color: Color(0xffff734c)),
-                    child: Text(
-                      "All",
-                      style: TextStyle(
-                        fontSize: 18.0,
-                        fontWeight: FontWeight.bold,
+                    margin: EdgeInsets.only(top: 20.0, bottom: 20.0),
+                    width: 80,
+                    decoration: BoxDecoration(
+                      color: Color(0xffff734c),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Center(
+                      child: Text(
+                        "All",
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 20.0),
+
+                  Container(
+                    width: 180,
+                    padding: EdgeInsets.all(10),
+                    child: Material(
+                      borderRadius: BorderRadius.circular(20),
+                      elevation: 3.0,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image.asset(
+                              "images/burger.png",
+                              height: 30.0,
+                              width: 30.0,
+                              fit: BoxFit.cover,
+                            ),
+                            Text(
+                              "Burger",
+                              style: TextStyle(
+                                fontSize: 18.0,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  SizedBox(width: 5.0),
+                  Container(
+                    width: 180,
+                    padding: EdgeInsets.all(10),
+                    child: Material(
+                      borderRadius: BorderRadius.circular(20),
+                      elevation: 3.0,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image.asset(
+                              "images/pizza.png",
+                              height: 30.0,
+                              width: 30.0,
+                              fit: BoxFit.cover,
+                            ),
+                            Text(
+                              "Pizza",
+                              style: TextStyle(
+                                fontSize: 18.0,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 5.0),
+
+                  Container(
+                    width: 180,
+                    padding: EdgeInsets.all(10),
+                    child: Material(
+                      elevation: 3.0,
+                      borderRadius: BorderRadius.circular(20),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image.asset(
+                              "images/pasta.png",
+                              height: 30.0,
+                              width: 30.0,
+                              fit: BoxFit.cover,
+                            ),
+                            Text(
+                              "Pasta",
+                              style: TextStyle(
+                                fontSize: 18.0,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                   ),
                 ],
               ),
             ),
+
+            SizedBox(height: 20.0,),
+
+            Container(
+
+              child: Column(
+
+                children: [
+
+
+                  
+                ],
+              ),
+            )
+
+
           ],
         ),
       ),
